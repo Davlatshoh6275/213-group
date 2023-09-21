ad86a6ef4618ff924f7fb9b5218fd625f8bb9861
Davlatshoh <davlatshoh6275@gmail.com>
2023-09-21 12:03:45 +0300

    finish lesson DOM

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let a = document
-let body = document.body
-let head = document.head
-let fchild = document.firstChild
-let lchild = document.lastChild
-let childNodes = document.childNodes
-let childrens = document.children
+// let a = document
+// let body = document.body
+// let head = document.head
+// let fchild = document.firstChild
+// let lchild = document.lastChild
+// let childNodes = document.childNodes
+// let childrens = document.children
 
 // console.log(a);
 // console.log(body);
@@ -14,19 +14,44 @@ let childrens = document.children
 // console.log(childNodes);
 // console.log(childNodes);
 
-let h1 = document.getElementsByTagName("h1")
-let text = document.getElementsByClassName("text")
-let textId = document.getElementById("text")
 
-console.log(textId);
+// console.log(textId);
 
 // console.log(text.item(1));
 
-// text.forEach(item => {
-//     console.log(item);
-// })
+
 
 
 
 // console.log(h1[2]);
 // console.log(text);
+// let h1 = document.getElementsByTagName("h1")
+// let text = document.getElementsByClassName("text")
+// let textId = document.getElementById("text")
+
+// let a = document.querySelector("h1")
+// let b = document.querySelector(".text")
+// let c = document.querySelector("#text")
+
+// let all = document.querySelectorAll(".text")
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(all);
+
+// let h = []
+
+// console.log(h);
+
+// all.forEach(item => {
+//     console.log(item);
+// })
+
+let hi = "Hello"
+
+let h1 = document.querySelector("h1")
+
+// h1.innerText = "salom"
+// h1.innerHTML = `<div>${hi}</div>`
+// h1.innerHTML = "Hello"

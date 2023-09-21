0434c46a136bbd37a595e93e78ff52b805ab6c56
Davlatshoh <davlatshoh6275@gmail.com>
2023-09-21 11:20:55 +0300

    first lesson Dom Js

---
+++ main.js
@@ -0,0 +1,32 @@
+let a = document
+let body = document.body
+let head = document.head
+let fchild = document.firstChild
+let lchild = document.lastChild
+let childNodes = document.childNodes
+let childrens = document.children
+
+// console.log(a);
+// console.log(body);
+// console.log(head);
+// console.log(fchild);
+// console.log(lchild);
+// console.log(childNodes);
+// console.log(childNodes);
+
+let h1 = document.getElementsByTagName("h1")
+let text = document.getElementsByClassName("text")
+let textId = document.getElementById("text")
+
+console.log(textId);
+
+// console.log(text.item(1));
+
+// text.forEach(item => {
+//     console.log(item);
+// })
+
+
+
+// console.log(h1[2]);
+// console.log(text);

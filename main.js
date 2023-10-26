f78b77dcf75dcdea2ad8e9122594a1417dab4cd3
Davlatshoh <davlatshoh6275@gmail.com>
2023-10-26 12:00:37 +0300

    lesson todo half end

--- main.js
+++ main.js
@@ -569,3 +569,53 @@
 //     getData()
 // })
 
+
+
+let inp = document.querySelector('.text')
+let addTodo = document.querySelector('.addTodo')
+let todoBox = document.querySelector('.todoBox')
+
+let count = 0
+
+let todo = {
+    title: ''
+}
+
+function getValue(obj) {
+    inp.addEventListener('keyup', (event) => {
+        let value = event.target.value
+
+        obj.title = value
+    })
+}
+
+getValue(todo)
+
+addTodo.addEventListener('click', (event) => {
+    event.preventDefault()
+    getValue(todo)
+    count++
+
+    let todoHtml = `
+    <li class="todo" id="${count}" >
+   ${todo.title}
+    <div>
+        <button data-delete="salom" class="delete">X</button>
+        <button class="change"><i class='bx bxs-chat'></i></button>
+    </div>
+  </li>`
+
+    todoBox.insertAdjacentHTML("beforeend", todoHtml)
+    inp.value = ''
+
+    
+
+})
+
+window.addEventListener('click', (event) => {
+    let btn = event.target.dataset.delete
+    if (btn) {
+        let todo = event.target.closest('.todo')
+        todo.remove()
+    }
+})
